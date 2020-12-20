8aa3c007d504be66b239c93c2c4bc247af9352f1
StephanieJolianis <stephaniejolianis@gmail.com>
2020-12-20 08:58:19 -0500

    organizacion router

--- src/App.js
+++ src/App.js
@@ -1,51 +1,43 @@
-// ----- Hoja de estilos --------
 import '../src/css/styles.css';
+// import "../src/sass/styles.scss";
+import Login from './components/Login';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-
+import { useState } from "react";
 // ----- Componentes --------
-import PageWaiter from '../src/components/pages/PageWaiter';
-import Login  from '../src/components/Login';
-import Layout from '../src/components/Layout';
-import Table from "../src/components/Table";
-import TakingOrder from "../src/components/TakingOrder";
-import NoFound  from '../src/components/NoFound';
+import Table from "./components/Table";
+import TakingOrder from "./components/TakingOrder";
 import Status from "./components/StatusOrder";
 import Stock from "./components/Stock";
 import Bill from "./components/Bill";
 import SelectTable from "./components/SelectTable";
+import NotFound from "./components/NotFound";
+import Layout from "./components/Layout";
 
-
-const App =() => {
-
-  let currentPath =window.location.pathname;
-
-  if (currentPath === '/'){
-    localStorage.clear('activeUser')
-  }
-
+const App = () => {
+// const [user, setUser]= useState("");
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Layout>
-              <Route exact path= "/pageWaiter/:user" component={PageWaiter}/>
-              <Route exact path="/table" component={Table} />
+            <div className="App">
+              {/* <div>{user}</div>
+              <NotFound cualquiercosa={setUser} /> */}
+              <Router>
+                <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/">
+                  <Layout>
+                <Route exact path="/table" component={Table} />
                 <Route exact path="/order/:id" component={TakingOrder} />
                 <Route exact path="/status" component={Status} />
                 <Route exact path="/bill" component={Bill} />
                 <Route exact path="/selecttable" component={SelectTable} />
                 <Route exact path="/stock" component={Stock} />
-          </Layout>
-          <Route path="/404" component={NoFound} />
-          <Redirect from="*" to="/404" />
-        </Switch>
-        
-      </Router> 
-    </div>
-  );
+                  </Layout>
+                  </Route>
+                  <Route path="*" component={NotFound} />
+                </Switch>
 
+              </Router> 
+            </div>
+  )
 }
-
 
 export default App;
