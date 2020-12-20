fc46009963cb0df6cb0ab7f3c35c986e2e13f637
StephanieJolianis <stephaniejolianis@gmail.com>
2020-12-20 08:44:08 -0500

    pruebas de estilo en modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,11 @@
+import { withRouter } from "react-router";
 import React, { useState } from 'react';
 
 // -------- Modal ------------->
 const Modal = (props) => {
-    let order = props.detailOrder;
-    let setStylesBtns = props.setStylesBtn;
-    let stylesBtns = props.stylesBtn;
+    //let order = props.detailOrder;
+    // let setStylesBtns = props.setStylesBtn;
+    // let stylesBtns = props.stylesBtn;
     let DetailProtein=[];
     let DetailAdicional=[];
     let DetailType = [];
@@ -24,6 +25,7 @@ const Modal = (props) => {
     //------------- Objeto para enviar a la orden--------->
     const objectOrder = {
         id:"",
+        idProduct:"",
         name:"",
         quantity:1,
         price:0,
@@ -33,12 +35,12 @@ const Modal = (props) => {
 
 
     if(props.detailProduct[0]){
-        objectProps.id = props.detailProduct[0].id;
+        objectProps.idProduct = props.detailProduct[0].id;
         objectProps.name = props.detailProduct[0].name;
         objectProps.price = props.detailProduct[0].price;
         objectOrder.name = objectProps.name;
         objectOrder.price = objectProps.price;
-        objectOrder.id = objectProps.id;
+        objectOrder.idProduct = objectProps.idProduct;
     if(props.detailProduct[0].custom.protein)
         objectProps.protein = props.detailProduct[0].custom.protein;
 
@@ -51,9 +53,7 @@ const Modal = (props) => {
 
 
     DetailProtein = objectProps.protein.map((item)=>{
-        return (<button onClick={()=> {addCustom(objectOrder, {name: item.name, price: item.price, id: item.id, property: "protein"}, true)
-        props.setStylesBtn({background: "red"})
-    }}  key={item.id} style={ props.stylesBtn }>{item.name}</button>)
+        return (<button onClick={()=> addCustom(objectOrder, {name: item.name, price: item.price, id: item.id, property: "protein"}, true)}  key={item.id} >{item.name}</button>)
     })
 
 
@@ -81,8 +81,9 @@ const Modal = (props) => {
                 {DetailType}
             </div>
                 <button onClick={()=> {
-                    order.push(objectOrder);
-                    props.setDetailProduct(order);
+                    props.modifyOrder({key:"create", value:objectOrder})
+                    // order.push(objectOrder);
+                    // props.setDetailProduct(order);
                     props.close(false);
                 }}>Agregar</button>
                 <button onClick={()=> props.close(false)}>Cancelar</button>
@@ -121,15 +122,29 @@ const addCustom=(orderObject, customFood, selSingle)=>{
 }
 
 
-const styleButtonCustom = (algo) => {
-if(algo.background == "yellow"){
-    algo.background = "red"
-}else{
-    algo.background = "yellow"
-}
-return algo;
-}
+// ---------- Style Buttons -------------->
+// const styleButtonCustom = (boolean, styleBtn) => {
+//     console.log(styleBtn);
+//     console.log(boolean);
+//     if(boolean==false){
+//         styleBtn.background = "red";
+//         boolean = true;
+//     }else{
+//         styleBtn.background = "yellow";
+//         boolean=false;
+//     }
+//     return styleBtn;
+// }
+
+// const styleButtonCustom = (algo) => {
+// if(algo.background == "yellow"){
+//     algo.background = "red"
+// }else{
+//     algo.background = "yellow"
+// }
+// return algo;
+// }
 
 
 
-export default Modal;    
+export default Modal; 
