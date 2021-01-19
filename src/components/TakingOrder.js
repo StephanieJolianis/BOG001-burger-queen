608b2b81ce81ffbf6cb3c7abdc12e382288847c5
StephanieJolianis <stephaniejolianis@gmail.com>
2021-01-19 07:25:50 -0500

    deploy version

--- src/components/TakingOrder.js
+++ src/components/TakingOrder.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useEffect } from 'react';
+import React, { useState, useReducer } from 'react';
 import { useParams } from "react-router-dom";
 //----------Componentes------------------------------
 import Breakfast from "./Breakfast";
